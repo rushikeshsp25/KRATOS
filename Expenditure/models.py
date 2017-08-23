288f7523ed9409a2fd84a55266f544d812f02965
rushikeshsp25 <rushikeshsp25@gmail.com>
2017-08-23 09:30:40 +0530

    index page updated(bullshit)

--- Expenditure/models.py
+++ Expenditure/models.py
@@ -5,10 +5,18 @@ from datetime import datetime
 class Debits(models.Model):
     product_name=models.CharField(max_length=250)
     quantity=models.IntegerField(default=0)
-    unit=models.CharField(max_length=10,default='kg')
+    unit=models.CharField(max_length=10,default='NA')   #NA -> Not Applied
     price=models.IntegerField(default=0)
     tax=models.BooleanField(default=0)       #1-GST 0-Not GST
-    system=models.IntegerField(default=0)    #0-misc 1-break 2-suspension 3-chasis 4-engine
+
+    #systems Fields#############################
+    sys_break=models.BooleanField(default=0)
+    sys_suspension = models.BooleanField(default=0)
+    sys_chasis = models.BooleanField(default=0)
+    sys_engine = models.BooleanField(default=0)
+    sys_misc = models.BooleanField(default=0)
+    ############################################
+
     category=models.IntegerField(default=0)  #0-other 1-cat1 2-cat2 etc.
     user=models.ForeignKey(User, default=1)
     date_time=models.DateTimeField(default=datetime.now, blank=True)
